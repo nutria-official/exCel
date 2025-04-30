839f3c7e6b1a80fb302909b2f94262c47a2c93c4
nutria-official <204664629+nutria-official@users.noreply.github.com>
2025-04-30 20:18:10 +0200

    Update backend.c
    RESET now also works.
    
    Added MATH, but no operations work yet.
    
    Added EXIT to close the program without making it  crash.
    
    Added BACK, but it does not work yet.

--- backend.c
+++ backend.c
@@ -9,7 +9,8 @@ void help();
 void commandList();
 void action();
 void read();
-
+void back();
+void mathOperationList();
 
 int main()
 {
@@ -18,7 +19,8 @@ int main()
     scanf("%d", &gridSize);
 
     float **cell = malloc(gridSize * sizeof(float *)); // Makes dynamic memory - I've gotta understand this better.
-    for (int i = 0; i < gridSize; i++) {
+    for (int i = 0; i < gridSize; i++)
+    {
         cell[i] = malloc(gridSize * sizeof(float));
     }
 
@@ -26,7 +28,7 @@ int main()
     return 0;
 }
 
-void action (float **cell, int gridSize)
+void action(float **cell, int gridSize)
 {
     enum commands
     {
@@ -34,7 +36,8 @@ void action (float **cell, int gridSize)
         MATH,
         READ,
         RESET,
-        HELP
+        HELP,
+        EXIT
     };
     int command;
 
@@ -59,10 +62,13 @@ void action (float **cell, int gridSize)
     case HELP:
         help();
         break;
+    case EXIT:
+        printf("exiting...\n");
+        exit(0);
+
     default:
         printf("Invalid input. Try again.\n");
-        main();
-        break;
+        action(cell, gridSize);
     }
 }
 void commandList()
@@ -73,6 +79,7 @@ void commandList()
     printf("2: READ\n");
     printf("3: RESET\n");
     printf("4: HELP\n");
+    printf("5: EXIT\n");
 }
 
 void input(float **cell, int gridSize)
@@ -85,13 +92,65 @@ void input(float **cell, int gridSize)
     scanf("%d", &col);
     printf("value:\n");
     scanf("%f", &value);
-    printf("cell[%d][%d] set to %f", row, col, value);
+
+    cell[row][col] = value;
+
+    printf("cell[%d][%d] set to %f\n", row, col, value);
+    action(cell, gridSize);
 }
 
 void math()
 {
-    printf("math has run\n"); // Placeholder.
+    enum mathOperations
+    {
+        ADD,
+        SUBTRACT,
+        MULTIPLY,
+        DIVIDE,
+        SUM,
+        BACK
+    };
+
+    int row, col, operation;
+    float newValue;
+
+    printf("row:\n");
+    scanf("%d", &row);
+    printf("col:\n");
+    scanf("%d", &col);
+    mathOperationList(&operation);
+    switch ((enum mathOperations)operation)
+    {
+    case ADD:
+        break;
+    case SUBTRACT:
+        break;
+    case MULTIPLY:
+        break;
+    case DIVIDE:
+        break;
+    case SUM:
+        break;
+    case BACK:
+        break;
+    default:
+        printf("Invalid input. Try again.\n");
+        mathOperationList(&operation);
+    }
 }
+
+void mathOperationList(int *operation)
+{
+    printf("operation list:\n");
+    printf("0: ADD\n");
+    printf("1: SUBTRACT\n");
+    printf("2: MULTIPLY\n");
+    printf("3: DIVIDE\n");
+    printf("4: SUM\n");
+    printf("5: BACK\n");
+    scanf("%p", &operation);
+}
+
 void read(float **cell, int gridSize)
 {
     int row, col;
@@ -100,19 +159,20 @@ void read(float **cell, int gridSize)
     scanf("%d", &row);
     printf("col:\n");
     scanf("%d", &col);
-    printf("cell[%d][%d] = %f", row, col, cell[row][col]);
+    printf("cell[%d][%d] = %f\n", row, col, cell[row][col]);
+    action(cell, gridSize);
 }
 void reset(float **cell, int gridSize)
 {
-    for(int i = 0; i < gridSize; i++)
+    for (int i = 0; i < gridSize; i++)
     {
         for (int j = 0; j < gridSize; j++)
         {
-           cell[i][j] = 0;
+            cell[i][j] = 0;
         }
-        
     }
     printf("All cells have been reset\n");
+    action(cell, gridSize);
 }
 
 void help()
